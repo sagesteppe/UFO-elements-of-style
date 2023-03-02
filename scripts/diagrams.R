b23e44cee9ba7efabea801573aa4039dc78e9ed3
sagesteppe <reedbenkendorf27@gmail.com>
2023-03-02 15:25:48 -0600

    added definitions to glossary and worked on conceptual figure

--- scripts/diagrams.R
+++ scripts/diagrams.R
@@ -1,21 +1,33 @@
 library(sf)
 library(tidyverse)
+setwd('/media/sagesteppe/ExternalHD/UFO_elements_of_style/scripts')
 
-
+set.seed(1)
 
 d <- data.frame(
-  'y' = sample(1:125, size = 50),
-  'x' = sample(1:125, size = 50),
-  'panel' = sample(rep(1:5, each = 10))
+  'y' = sample(3:1000, size = 50),
+  'x' = sample(3:1000, size = 50),
+  'panel' = paste(rep(LETTERS[1:5], each = 10), '-', rep(2020:2024, each = 10))
 )
 
-ggplot(d, aes(x = x,y = y, color = as.character(panel))) +
-  geom_jitter(width = 20) +
+ggplot(d, aes(x = x, y = y, color = panel,
+              shape = panel)) +
+  geom_jitter(width = 50, height = 50, size = 2) +
   theme_bw() +
+  scale_color_manual(values = 
+                       RColorBrewer::brewer.pal(n = 5, name = 'Dark2')) +
+  scale_shape_manual('Panel', values=c(15:19)) +
   labs(title = "A sample frame with five panels", color = 'Panel',
-       x = NULL, y = NULL) +
-  theme(plot.title = element_text(hjust = 0.5),
-        axis.text=element_blank(), axis.ticks=element_blank(),
-        panel.grid.minor = element_blank(), panel.grid.major = element_blank()
-        )
+       x = 'Longitude (m)', y = 'Latitude (m)') +
+  theme(
+    aspect.ratio = 1,
+    plot.title = element_text(hjust = 0.5),
+    legend.title = element_text(hjust = 0.5),
+    ) +
+  lims(x = c(0,1000), y = c(0,1000))
 
+ggsave('../results/plots/SampleFrameExample.png', dpi = 300, height = 10, width = 10
+       , units ="cm", 
+       device = "png")
+
+rm(d)
