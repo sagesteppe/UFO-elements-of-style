17e10a0d86206822a8b3814f5824a2a56af5e685
sagesteppe <reedbenkendorf27@gmail.com>
2023-03-21 14:17:03 -0600

    added spelling to numbers

--- scripts/num_pr.R
+++ scripts/num_pr.R
@@ -1,15 +1,16 @@
 num_pr <- function(x){
-  if(x > 10){x = x} else
-    if(x == 9){x = 'nine'} else
-      if(x == 8){x = 'eight'} else
-        if(x == 7){x = 'seven'} else
-          if(x == 6){x = 'six'} else
-            if(x == 5){x = 'five'} else
-              if(x == 4){x = 'four'} else
-                if(x == 3){x = 'three'} else
-                  if(x == 2){x = 'two'} else
-                    if(x == 1){x = 'one'} else
-                      if(x == 0){x = 'no'} else
-                        if(x < 0){x = x}
-  print(x)
+  if(x > 10){y = x} else
+    if(x == 9){y = 'nine'} else
+      if(x == 8){y = 'eight'} else
+        if(x == 7){y = 'seven'} else
+          if(x == 6){y = 'six'} else
+            if(x == 5){y = 'five'} else
+              if(x == 4){y = 'four'} else
+                if(x == 3){y = 'three'} else
+                  if(x == 2){y = 'two'} else
+                    if(x == 1){y = 'one'} else
+                      if(x == 0){y = 'no'} else
+                        if(x < 0){y = x}
+  return(y)
 }
+
